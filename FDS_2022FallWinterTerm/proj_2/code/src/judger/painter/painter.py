7a932625f6c4d03fd9c3c268fce7717735e6e464
Isshiki_Hugh <2511524185@qq.com>
2022-10-22 00:54:11 +0800

    implement section A

--- FDS_2022FallWinterTerm/proj_2/code/src/judger/painter/painter.py
+++ FDS_2022FallWinterTerm/proj_2/code/src/judger/painter/painter.py
@@ -1,17 +1,17 @@
-import matplotutils.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import warnings
 
 def foo():
     warnings.filterwarnings('ignore')
     # read in the data
-    xA = []
-    yA = []
-    xB = []
-    yB = []
     with open("txt", "r") as f:
         T = int(f.readline())
         while T != 0:
+            xA = []
+            yA = []
+            xB = []
+            yB = []
             num = int(f.readline())
             for i in range(num):
                 line = f.readline().split()
@@ -29,24 +29,8 @@ def foo():
             xB.append(xB[0])
             yB.append(yB[0])
 
-    plt.plot(xA, yA, color='g', marker='o', label='N3')
-    plt.plot(xB, yB, color='g', marker='o', label='N3')
-    plt.legend()
-    plt.show()
-    # plt.savefig("N3vsN4.jpg")
-
-    # # draw picture of N3 vs N4 vs N6
-    # plt.figure(figsize=(4, 8))
-    # plt.xlim(5, 100)
-    # plt.xlabel("N")
-    # plt.ylim(0, 0.5*(arrT[2][len(arrN)-1]+arrT[2][len(arrN)-2]))
-    # plt.ylabel("run_time/s")
-    # plt.plot(arrN, arrT[0], color='g', marker='o', label='N3')
-    # plt.plot(arrN, arrT[1], color='r', marker='o', label='N4')
-    # plt.plot(arrN, arrT[2], color='b', marker='o', label='N6')
-    # plt.legend()
-    # plt.savefig("N3vsN4vsN6.jpg")
-
-    # plt.show()
-
+            for i in range(num):
+                plt.scatter(xA[i], yA[i], color=(0.1, (i+2)/(num+5), (i+2)/(num+5)), )    
+                plt.plot([xA[i], xA[i+1]], [yA[i], yA[i+1]], color=(0.1, (i+2)/(num+5), (i+2)/(num+5)))       
+            plt.show()
 foo()
--- FDS_2022FallWinterTerm/proj_2/code/src/judger/section_A/maker.cpp
+++ FDS_2022FallWinterTerm/proj_2/code/src/judger/section_A/maker.cpp
@@ -2,18 +2,118 @@
 #include <fstream>
 #include <cstdlib>
 #include <vector>
+#include <random>
 
 #include "../../lib/meta_data.h"
 
 using namespace std;
 
+#define CANVAS_SIZE 128
+#define STEP_LEN 4
+#define STEP_LEN_LOWER_LIM 0.1
+#define PI 3.1415926536
+
+// Hyperparameter
+const double weakenRate = 0.2;
+
+default_random_engine e(time(NULL));
+
+// Get a random number in -1 ~ 1 according to normal distribution.
+double getNormalFactor(){
+    static normal_distribution<double> distr(0.0, 0.5);
+    double factor;
+    do {
+        factor = distr(e);
+    } while(abs(factor) > 5);
+    return factor / 5;
+}
+
+// Get a random number in 0 ~ 1 according to normal distribution.
+double getAbsNormalFactor(){
+    return abs(getNormalFactor());
+}
+
+// Get a random number in -1 ~ 1 according to uniform distribution.
+double getUniformFactor(){
+    static uniform_real_distribution<double> distr(-1.0, 1.0);
+    return distr(e);
+}
+
+double getAbsUniformFactor(){
+    return abs(getUniformFactor());
+}
+
+Point2D randomMove(Point2D & base){
+    double step = STEP_LEN * ( 1 - getAbsNormalFactor()) + STEP_LEN_LOWER_LIM;
+    double angle = PI * getUniformFactor();
+    double dx = step * cos(angle);
+    double dy = step * sin(angle);
+    return(Point2D(
+        base.getX() + dx,
+        base.getY() + dy
+    ));
+}
+
+Point2D randomTendentMove(Point2D & base, Point2D & preBase, double offsetAngle, Point2D & end, double backRate, double restTasks){
+    double dx = base.getX() - preBase.getX();
+    double dy = base.getY() - preBase.getY();
+    double baseAngle = acos( dx/sqrt(dx*dx+dy*dy) );
+    if(dy < 0) baseAngle = 2 * PI - baseAngle;
+
+    double step = STEP_LEN * ( 1 - getAbsNormalFactor()) + STEP_LEN_LOWER_LIM;
+    double angle = offsetAngle + baseAngle + PI * getNormalFactor();
+    dx = step * cos(angle);
+    dy = step * sin(angle);
+    if(abs(dx) > STEP_LEN || abs(dy) > STEP_LEN) cout << dx << dy;
+    return(Point2D(
+        (1 - backRate) * (base.getX() + dx) + backRate * (end.getX() - base.getX()) / max(restTasks-2, 1.2),
+        (1 - backRate) * (base.getY() + dy) + backRate * (end.getY() - base.getY()) / max(restTasks-2, 1.2)
+    ));
+}
+
 Polygon2D randPg(int size){
+    double offsetAngle = 2 * PI / ( size * 1.05 ); 
+    std::vector<Point2D> pts;
     
+    pts.clear();
+    double x, y;
+    auto base = Point2D(0, 0);
+    pts.push_back( base );     // Base point.
+    pts.push_back( randomMove( base ) );     // Base point.
+    
+    while(pts.size() < size){
+        auto base = pts[ pts.size()-1 ];
+        auto preBase = pts[ pts.size()-2 ];
+        
+        double backRate = pow((double)pts.size() / size, 16) 
+            * weakenRate 
+            * min(
+                1.0,
+                pow( max(
+                    base.getDis(pts[0]) / STEP_LEN / 8,
+                    0.0
+                ), 0.1)
+            );
+
+        int repeatCnt = 0;
+        Point2D newPts(0,0);
+        do{
+            ++repeatCnt;
+            newPts = randomTendentMove(base, preBase, offsetAngle, pts[0], pow(backRate, 1.0/repeatCnt), size - pts.size());
+        }while( abs(newPts.getX()) > CANVAS_SIZE || abs(newPts.getY()) > CANVAS_SIZE );
+    
+        pts.push_back( Point2D(
+            newPts.getX(),
+            newPts.getY()
+        ) );
+    }
+
+    return Polygon2D(pts);
 }
 
 
 Polygon2D rigidTransPg(Polygon2D & base, int offset){
-
+    return base;
 }
 
 int main(int argc, char * argv[]){
@@ -27,6 +127,7 @@ int main(int argc, char * argv[]){
     sizeSeq.push_back(3);
     sizeSeq.push_back(5);
     sizeSeq.push_back(10);
+    sizeSeq.push_back(25);
     sizeSeq.push_back(50);
     sizeSeq.push_back(100);
 
